3799ecffe1842b5c621dfd55e8ebdee0a70369fe
szombathrobert4 <szombathrobert4@gmail.com>
2024-02-15 14:33:35 +0100

    Random Emojis

---
+++ 21. Random Emojis/app.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('#emoji')
+
+const emojis = [
+    "😀",
+    "🤣",
+    "😎",
+    "🤩",
+    "😆",
+    "🤐",
+    "😍",
+    "🙄",
+    "😑",
+    "🤑",
+    "😤",
+    "😙",
+    "😨",
+    "😰",
+    "🥴",
+    "🤡",
+    "🤬",
+];
+
+btn.addEventListener('mouseover', () => {
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)]
+})
