b2900cc791fd23bb4ec71388b27c9f06dbaa7109
szombathrobert4 <szombathrobert4@gmail.com>
2024-02-15 09:56:54 +0100

    Heart

---
+++ 14. Heart/app.js
@@ -0,0 +1,12 @@
+let grayHeart = document.querySelector('.gray-heart')
+let redHeart = document.querySelector('.red-heart')
+
+grayHeart.addEventListener('click', () => {
+    redHeart.classList.add('animation')
+    grayHeart.classList.add('fill-color')
+})
+
+redHeart.addEventListener('click', () => {
+    redHeart.classList.remove('animation')
+    grayHeart.classList.remove('fill-color')
+})
