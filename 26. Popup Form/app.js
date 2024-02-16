54c4e21b3521ccb629fe4e33b74e09cb443cec5b
szombathrobert4 <szombathrobert4@gmail.com>
2024-02-16 14:18:45 +0100

    Popup Form

---
+++ 26. Popup Form/app.js
@@ -0,0 +1,15 @@
+// closeBtn
+const close = document.getElementById('close')
+// openBtn
+const open = document.getElementById('open')
+// modal
+const modal = document.getElementById('modal')
+
+
+open.addEventListener('click', () => modal.classList.add('show-modal'))
+
+close.addEventListener('click', () => modal.classList.remove('show-modal'))
+
+window.addEventListener('click', e => {
+    e.target === modal ? modal.classList.remove('show-modal') : false;
+})
