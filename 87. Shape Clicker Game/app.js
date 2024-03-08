6da110c7651d0b67b4bd5b89e0eb764f24fc8b8e
szombathrobert4 <szombathrobert4@gmail.com>
2024-03-08 12:20:42 +0100

    Shape Clicker Game

---
+++ 87. Shape Clicker Game/app.js
@@ -0,0 +1,48 @@
+const game = {
+    timer: 0,
+    start: null
+};
+
+// Create message element
+const message = document.createElement('div')
+message.classList.add("message");
+message.textContent = "Press To Start";
+document.body.prepend(message);
+
+// Create box
+
+const box = document.createElement("div")
+box.classList.add("box")
+
+const output = document.querySelector('.output')
+output.append(box);
+
+box.addEventListener('click', () => {
+    box.textContent = ""
+    box.style.display = 'none'
+    game.timer = setTimeout(addBox, randomNumbers(3000))
+    if(!game.start) {
+        message.textContent = 'Watch for element and click it!'
+    } else {
+        const current = new Date().getTime()
+        const duration = (current - game.start) / 1000
+        message.textContent = `It took ${duration} second to click`
+    }
+})
+
+function randomNumbers(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function addBox() {
+    game.start = new Date().getTime()
+    const container = output.getBoundingClientRect()
+    const dim = [randomNumbers(50) + 20, randomNumbers(50) + 20];
+    box.style.display = 'block'
+    box.style.width = `${dim}px`
+    box.style.height = `${dim[1]}px`
+    box.style.backgroundColor = "#" + Math.random().toString(16).substr(-6)
+    box.style.left = randomNumbers(container.width - dim[0]) + "px"
+    box.style.top = randomNumbers(container.height - dim[1]) + "px"
+    box.style.borderRadius = randomNumbers(50) + '%'
+}
